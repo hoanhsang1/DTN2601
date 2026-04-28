d650a8d11bc56b3cd3b6db46bd023066ede2191c
AnhSang <hoanhsang24@gmail.com>
2026-04-28 19:09:50 +0700

    Assignment_3

--- Assignment_JavaCore/Assignment_3/src/ExerciseThree.java
+++ Assignment_JavaCore/Assignment_3/src/ExerciseThree.java
@@ -1,25 +1,34 @@
+import Entity.Question;
+
 public class ExerciseThree {
+//    public void QuestionOne() {
+//        Integer salary = 5000;
+//
+//        float f = salary.floatValue();
+//
+//        System.out.printf("Salary: %.2f\n", f);
+//    }
+//
+//    public void QuestionTwo() {
+//        String str = "1234567";
+//
+//        int num = Integer.parseInt(str);
+//
+//        System.out.println("Int: " + num);
+//    }
+//
+//    public void QuestionThree() {
+//        Integer num = 1234567;
+//
+//        int primitive = num; // unboxing
+//
+//        System.out.println("Primitive int: " + primitive);
+//    }
+
     public void QuestionOne() {
-        Integer salary = 5000;
-
-        float f = salary.floatValue();
-
+        Integer Salary = 5000;
+        Float SalaryPercent = Salary.floatValue();
         System.out.printf("Salary: %.2f\n", f);
-    }
 
-    public void QuestionTwo() {
-        String str = "1234567";
-
-        int num = Integer.parseInt(str);
-
-        System.out.println("Int: " + num);
-    }
-
-    public void QuestionThree() {
-        Integer num = 1234567;
-
-        int primitive = num; // unboxing
-
-        System.out.println("Primitive int: " + primitive);
     }
 }
